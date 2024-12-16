91939ab43709cabcf8d61dc1b7239f9ef474675b
TinfoilyTim <tim.erlandsson01@gmail.com>
2024-12-16 15:47:40 +0100

    Initial commit

---
+++ src/biblotek/Biblotek.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblotek;
+
+/**
+ *
+ * @author 05timerl
+ */
+public class Biblotek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bok book = new Bok();
+        
+        System.out.println(book.isbn);
+    }
+    
+}
---
+++ src/biblotek/Bok.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblotek;
+
+/**
+ *
+ * @author 05timerl
+ */
+public class Bok {
+    String title;
+    String author;
+    String isbn;
+    boolean stock;
+    
+    public Bok(){
+        this.title = "To the Lighthouse";
+        this.author = "Virginia Woolf";
+        this.isbn = "978-0141183411";
+        this.stock = false;
+        
+        
+    }
+}
